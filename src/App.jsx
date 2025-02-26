5bdbdb069e5f9f9a279a1499784db074a563c445
Aldana Tamara Di Flavio <adiflavio@unlam.edu.ar>
2025-02-26 17:17:03 -0300

    Inicio de proyecto base de Creacion de memes

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>
+    </>
+  )
+}
+
+export default App
