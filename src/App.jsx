64aba73520a4322c360dca6c587f6d61c95022ee
Aldana Tamara Di Flavio <adiflavio@unlam.edu.ar>
2025-02-26 18:00:28 -0300

    creacion del formulario y las funciones para escribir en los inputos y que muestre el texto en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+
+  const escribirLinea1 = (e) => {
+    setLinea1(e.target.value);
+  }
+
+  const escribirLinea2 = (e) => {
+    setLinea2(e.target.value);
+  }
 
   return (
     <>
@@ -16,6 +25,16 @@ function App() {
         <option value="philosoraptor">Philosoraptor</option>
         <option value="smart">Smart Guy</option>
       </select>
+      <br />
+      
+      <input onChange={escribirLinea1} type="text" placeholder="Linea 1" /><br />
+      <input onChange={escribirLinea2} type="text" placeholder="Linea 2" /><br />
+      <button>Exportar</button>
+      <div>
+        <span>{linea1}</span><br />
+        <span>{linea2}</span>
+        <img src="" />
+      </div>
     </>
   )
 }
